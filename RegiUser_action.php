c2b0a00dde3193156820ec2d480cc7e4c34f9369
mnngy01 <78626832+mnngy01@users.noreply.github.com>
2023-12-06 23:45:58 +0900

    Add files via upload

--- RegiUser_action.php
+++ RegiUser_action.php
@@ -9,12 +9,17 @@
 <body>
     <h2>사용자 등록하기</h2> <hr>
 
-<form method="get" action="RegisterUser.php">
+<form method="get" action="RegiUser_action.php">
 
     <table>
         <tr>
             <td><label for="name">이름</label></td>
             <td><input type="varchar(20)" name="name" id="name"></td>
+        </tr>
+
+        <tr>
+            <td><label for="studentNum">학번</label></td>
+            <td><input type="integer" name="studentNum" id="studentNum"></td>
         </tr>
 
         <tr>
@@ -47,21 +52,23 @@
     $progress = isset($_GET['progress']) ? $_GET['progress'] : '';
     $studentNum = isset($_GET['studentNum']) ? $_GET['studentNum'] : '';
 
-
-    $query = "INSERT INTO contest_exhibit (name, majorID, email, progress, uid)
-    values('$name', '$majorID', '$email', '$progress', '$studentNum')";
-    $result = $connect->query($query);
-
-    if (!$result){
-        ?>
-        <script> alert('사용자 등록이 완료되었습니다.'); location.href=".."; </script>
-        <?php
+    if (!empty($studentNum)) {
+   
+    
+        # 수정된 부분: 데이터베이스에 데이터 삽입
+        $sql = "INSERT INTO user (name, majorID, email, progress, uid)
+                  VALUES ('$name', '$majorID', '$email', '$progress', '$studentNum')";
+    
+        $result = $connect->query($sql);
+    
+        if ($result) { ?>
+            <script> alert('사용자 등록이 완료되었습니다.'); location.href = "regiUser_action.php"; </script>
+        <?php } else { echo "Error: " . mysqli_error($connect); ?>
+            <script> alert('사용자 등록에 실패했습니다. \n다시 시도해 주세요'); location.href = "regiUser_action.php"; </script>
+        <?php }
     } else {
-        echo "Error: " . mysqli_error($connect);
-        ?>
-        <script> alert('사용자 등록에 실패했습니다. \n다시 시도해 주세요'); location.href=".."; </script>
-    <?php 
-    } 
+        die(" ");
+    }
 ?>
 
 </body>
