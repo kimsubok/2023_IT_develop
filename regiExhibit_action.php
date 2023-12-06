d13caa4d9c194c3ecf7f29908808a71a37e6d38b
kimsubok <132457756+kimsubok@users.noreply.github.com>
2023-12-06 12:56:07 +0900

    Update regiExhibit_action.php

--- regiExhibit_action.php
+++ regiExhibit_action.php
@@ -9,64 +9,73 @@
 <body>
     <h2>공모전 등록하기</h2> <hr>
 
-<form method="get" action="regiExhibit_action.php">
+    <form method="post" action="regiExhibit_action.php">
 
-    <table>
-        <tr>
-            <td><label for="title">공모전이름</label></td>
-            <td><input type="text" name="title" id="title"></td>
-        </tr>
+        <table>
+            <tr>
+                <td><label for="title">공모전 이름</label></td>
+                <td><input type="text" name="title" id="title" required></td>
+            </tr>
 
-        <tr>
-            <td><label for="area">분야</label></td>
-            <td><input type="text" name="area" id="area"></td>
-        </tr>
+            <tr>
+                <td><label for="area">분야</label></td>
+                <td><input type="text" name="area" id="area" required></td>
+            </tr>
 
-        <tr>
-            <td><label for="com_date">모집완료일</label></td>
-            <td><input type="date" name="com_date" id="com_date"></td>
-        </tr>
+            <tr>
+                <td><label for="com_date">모집 완료일</label></td>
+                <td><input type="date" name="com_date" id="com_date" required></td>
+            </tr>
 
-        <tr>
-            <td><label for="host">주최측</label></td>
-            <td><input type="text" name="host" id="host"></td>
-        </tr>
+            <tr>
+                <td><label for="host">주최측</label></td>
+                <td><input type="text" name="host" id="host" required></td>
+            </tr>
 
-        <tr>
-            <td><label for="content">내용</label></td>
-            <td><input type="text" name="content" id="content"></td>
-        </tr>
-    </table>
-    
-    <button type="submit" class="btn btn-warning" stype="float:right;"
-        id="submit">제출하기</button>
-</form>
+            <tr>
+                <td><label for="content">내용</label></td>
+                <td><input type="text" name="content" id="content" required></td>
+            </tr>
+        </table>
 
-<?php
-    include 'connect.php'; // mysql이랑 연결해주는 php파일 
+        <button type="submit" class="btn btn-warning" style="float:right;" id="submit">제출하기</button>
+    </form>
 
-    // 데이터 읽어오기
-    $title = isset($_GET['title']) ? $_GET['title'] : '';
-    $area = isset($_GET['area']) ? $_GET['area'] : '';
-    $com_date = isset($_GET['com_date']) ? date('Y-m-d', strtotime($_GET['com_date'])) : '';
-    $host = isset($_GET['host']) ? $_GET['host'] : '';
-    $content = isset($_GET['content']) ? $_GET['content'] : '';
+    <?php
 
-    $sql = "INSERT into contest_exhibit (title, area, com_date, host, content)
-              values('$title', '$area', '$com_date', '$host', '$content')";
-    $result = $connect->query($sql);
+# 데이터 읽어오기
+$title = isset($_POST['title']) ? $_POST['title'] : '';
+$area = isset($_POST['area']) ? $_POST['area'] : '';
+$com_date = isset($_POST['com_date']) ? $_POST['com_date'] : '';
+$host = isset($_POST['host']) ? $_POST['host'] : '';
+$content = isset($_POST['content']) ? $_POST['content'] : '';
 
-    if ($result) {
-        ?>
-        <script> alert('공모전 등록이 완료되었습니다.'); location.href=".."; </script>
-        <?php
-    } else {
-        echo "Error: " . mysqli_error($connect); // 에러 메시지 출력
-        ?>
-        <script> alert('공모전 등록에 실패했습니다. \n다시 시도해 주세요'); location.href=".."; </script>
-        <?php
-    }
-    ?>
+# 데베 연결 정보
+$hostname = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "autoing";
+
+$con = mysqli_connect($hostname, $username, $password, $dbname) or die("Can't access DB");
+
+if (!empty($com_date) && strtotime($com_date)) {
+    $com_date = date("Y-m-d", strtotime($com_date));
+
+    # 수정된 부분: 데이터베이스에 데이터 삽입
+    $query = "INSERT INTO contest_exhibit (title, area, com_date, host, content)
+              VALUES ('$title', '$area', '$com_date', '$host', '$content')";
+
+    $result = mysqli_query($con, $query);
+
+    if ($result) { ?>
+        <script> alert('공모전 등록이 완료되었습니다.'); location.href = "/"; </script>
+    <?php } else { ?>
+        <script> alert('공모전 등록에 실패했습니다. \n다시 시도해 주세요'); location.href = "/"; </script>
+    <?php }
+} else {
+    die("Invalid date provided");
+}
+?>
+
 </body>
 </html>
-
